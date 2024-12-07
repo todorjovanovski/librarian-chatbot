2d09c11e2fb4374c4c1171fe4986178eb6fdeddf
Vedran <vedrandeskovski5@gmail.com>
2024-12-08 00:12:50 +0100

    adding the text prompt in text_generation_utils.py

--- back-end/text_generation_utils.py
+++ back-end/text_generation_utils.py
@@ -8,7 +8,16 @@ pc = Pinecone(
     )
 index = pc.Index("books")
 
-def get_similarity_by_query(query, file, k):
+def get_similarity_by_query(query, file, k=3):
     vector_store = get_vector_store()
     result = vector_store.similarity_search(query, k=k, filter={"source": file})
     return result
+
+def get_prompt_template():
+    prompt = PromptTemplate(
+        template="""
+        Based on the context below, answer only the question asked.
+        If you can't find the answer in the context, say that you don't have any knowledge:\n\nContext: {context}\n\nQuestion: {question}\n\nAnswer:""",
+        input_variables=["context", "question"],
+    )
+    return prompt
