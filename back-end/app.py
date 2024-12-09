bec2bb2785c1b72d9407a3e1fc629fa1e28008d6
Vedran <vedrandeskovski5@gmail.com>
2024-12-09 14:31:25 +0100

    implementing POST method for uploading a file to pinecone database

--- back-end/app.py
+++ back-end/app.py
@@ -1,14 +1,25 @@
 import os
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
 from fastapi.responses import JSONResponse
 from tempfile import NamedTemporaryFile
 import uvicorn
+from pinecone_utils import add_file_to_database
 
 app = FastAPI()
 
-@app.get("/hi")
-async def hi():
-    return {"message": "hi"}
+@app.post("/upload_book")
+async def upload_pdf(pdf: UploadFile = File(...), title: str = Form(...)):
+    if not pdf.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Books must be uploaded only in PDF format.")
+    
+    with NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(await pdf.read())
+        temp_file.flush()
+        temp_path = temp_file.name
+        add_file_to_database(file=temp_path)
+    os.remove(temp_path)
+
+    return JSONResponse(content={"title":title})
 
 
 if __name__ == "__main__":
