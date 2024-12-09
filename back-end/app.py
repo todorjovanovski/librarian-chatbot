6e6dafdc951fa997bf0b525d827c4d38a10ff05d
Vedran <vedrandeskovski5@gmail.com>
2024-12-09 14:43:23 +0100

    implemeting POST method for user question and returning a generated answer

--- back-end/app.py
+++ back-end/app.py
@@ -4,16 +4,16 @@ from fastapi.responses import JSONResponse
 from tempfile import NamedTemporaryFile
 import uvicorn
 from pinecone_utils import add_file_to_database
-
+from text_generation_utils import get_generated_text
 app = FastAPI()
 
 @app.post("/upload_book")
-async def upload_pdf(pdf: UploadFile = File(...), title: str = Form(...)):
-    if not pdf.filename.endswith(".pdf"):
+async def upload_book(book: UploadFile = File(...), title: str = Form(...)):
+    if not book.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="Books must be uploaded only in PDF format.")
     
     with NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-        temp_file.write(await pdf.read())
+        temp_file.write(await book.read())
         temp_file.flush()
         temp_path = temp_file.name
         add_file_to_database(file=temp_path)
@@ -21,6 +21,10 @@ async def upload_pdf(pdf: UploadFile = File(...), title: str = Form(...)):
 
     return JSONResponse(content={"title":title})
 
+@app.post("/ask")
+async def ask_question(question: str = Form(...), book: str = Form(...)):
+    answer = get_generated_text(query=question, file=book)
+    return JSONResponse(content={"answer":answer})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
