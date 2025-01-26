d5a12f55d87b7d6a49895fb8714c7b277fcc5b3e
Vedran <vedrandeskovski5@gmail.com>
2025-01-26 19:22:39 +0100

    adding entry validity for title

--- maui-chatbot/Librarian/ViewModels/UploadViewModel.cs
+++ maui-chatbot/Librarian/ViewModels/UploadViewModel.cs
@@ -3,7 +3,6 @@ using CommunityToolkit.Mvvm.Input;
 using Librarian.Pages;
 using Librarian.Services;
 using Librarian.ViewModels.Base;
-
 namespace Librarian.ViewModels;
 
 public partial class UploadViewModel : ViewModelBase
@@ -11,31 +10,35 @@ public partial class UploadViewModel : ViewModelBase
     private readonly INavigationService _navigationService;
     private readonly IChatService _chatService;
 
-    public string PdfName 
+    public string PdfName
         => PdfFile is null ? string.Empty : PdfFile.FileName;
-    
-    public ImageSource UploadPdfIcon 
+
+    public ImageSource UploadPdfIcon
         => PdfFile is null ? ImageSource.FromFile("attachment_icon") : ImageSource.FromFile("new_chat_icon");
 
     public string UploadPdfLabel
         => PdfFile is null ? "Upload Pdf" : "Start new chat";
 
-    [ObservableProperty] 
+    [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(PdfName), nameof(UploadPdfIcon), nameof(UploadPdfLabel))]
     private FileResult? _pdfFile;
-    
-    [ObservableProperty] 
+
+    [ObservableProperty]
     private string _chatTitle = string.Empty;
+
+    [ObservableProperty]
+    private bool _errorMessage = false;
 
     public UploadViewModel(INavigationService navigationService, IChatService chatService)
     {
         _navigationService = navigationService;
         _chatService = chatService;
     }
-    
+
     [RelayCommand]
     private async Task ChooseFile()
     {
+        ErrorMessage = false;
         if (PdfFile is null)
         {
             try
@@ -45,7 +48,7 @@ public partial class UploadViewModel : ViewModelBase
                     PickerTitle = "Choose a PDF file to upload.",
                     FileTypes = FilePickerFileType.Pdf
                 };
-            
+
                 PdfFile = await MainThread.InvokeOnMainThreadAsync(() => FilePicker.Default.PickAsync(options));
             }
             catch (Exception ex)
@@ -55,6 +58,11 @@ public partial class UploadViewModel : ViewModelBase
         }
         else
         {
+            if (string.IsNullOrWhiteSpace(ChatTitle))
+            {
+                ErrorMessage = true;
+                return;
+            }
             var newChat = await _chatService.StartNewChat(ChatTitle);
             PdfFile = null;
             ChatTitle = string.Empty;
@@ -66,8 +74,15 @@ public partial class UploadViewModel : ViewModelBase
     }
 
     [RelayCommand]
+    private void ClosePdf()
+    {
+        PdfFile = null;
+    }
+
+    [RelayCommand]
     private async Task GoToAllChats()
     {
+        ErrorMessage = false;
         await _navigationService.GoToAsync(nameof(AllChatsPage));
     }
 }
